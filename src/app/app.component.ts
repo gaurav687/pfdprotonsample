80b1a5051215221f867c17f001a2ce77a013827a
Gaurav Dhasal <gaurav.dhasal@emorphis.in>
2022-06-08 18:23:29 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit, ViewChild, ElementRef, asNativeElements } from '@angular/core';
+import WebViewer from '@pdftron/webviewer';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('viewer')
+  viewerRef!: ElementRef;
+  ngAfterViewInit(): void {
+    WebViewer({
+      path: '../assets/lib',
+      initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+      
+    }, this.viewerRef.nativeElement)
+  }
+  title = 'pdftron';
 }
