8f34718dee5468dfc5158fe5fc0a45a527bd88a7
Gaurav Dhasal <gaurav.dhasal@emorphis.in>
2022-06-10 15:59:02 +0530

    new module update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Component, AfterViewInit, ViewChild, ElementRef, asNativeElements } from '@angular/core';
-import WebViewer from '@pdftron/webviewer';
+// import WebViewer from '@pdftron/webviewer';
+import PSPDFKit from "pspdfkit";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-  @ViewChild('viewer')
-  viewerRef!: ElementRef;
-  ngAfterViewInit(): void {
-    WebViewer({
-      path: '../assets/lib',
-      initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
-      
-    }, this.viewerRef.nativeElement)
-  }
+  // @ViewChild('viewer')
+  // viewerRef!: ElementRef;
+ 
+  ngAfterViewInit() {
+		PSPDFKit.load({
+			// Use the assets directory URL as a base URL. PSPDFKit will download its library assets from here.
+			baseUrl: location.protocol + "//" + location.host + "/assets/",
+			document: "/assets/Document.pdf",
+			container: "#pspdfkit-container",
+		}).then(instance => {
+			// For the sake of this demo, store the PSPDFKit for Web instance
+			// on the global object so that you can open the dev tools and
+			// play with the PSPDFKit API.
+			(window as any).instance = instance;
+		});
+	}
   title = 'pdftron';
 }
